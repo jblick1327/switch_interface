ecbc7036a8eaaf4604bcd3edde20a6dec6f46e9a
jblick1327 <jblick1327@gmail.com>
2025-06-17 18:10:17 -0400

    fixed ignore error hopefully

--- pc_control.py
+++ pc_control.py
@@ -1,2 +1,8 @@
+import pynput
+
 def trigger_press():
     print("Press Detected")
+    #placeholder
+
+#basic predictive text (letter and/or word), n-grams are probably fine for now
+#linear and line-based scan
