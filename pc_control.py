87b84c21cbd0d91f8c6fc4d03192c0ea7b99abf6
jblick1327 <jblick1327@gmail.com>
2025-06-18 16:49:04 -0400

    starting to bridge pynput and keyboard gap

--- pc_control.py
+++ pc_control.py
@@ -1,8 +1,17 @@
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key as PynKey, Controller
+
+kb = Controller()
 
 def trigger_press():
     print("Press Detected")
     #placeholder
+
+def gui_to_controller(key):
+    if key.action is None:
+        kb.press(key.label)
+        kb.release(key.label)
+    else:
+        print("keep working")
 
 #basic predictive text (letter and/or word), n-grams are probably fine for now
 #linear and line-based scan
