2b5acefa0dbe1e65b2a73dfa739d26f17a5bc6f7
jblick1327 <jblick1327@gmail.com>
2025-06-18 12:59:35 -0400

    improved json template

--- kb_layout_io.py
+++ kb_layout_io.py
@@ -1,3 +1,22 @@
 import json
-from pathlib import Path
 from kb_layout import Key, KeyboardRow, KeyboardPage, Keyboard
+
+FOLDER = None
+FILE = 'layouts/basic_test.json'
+
+with open(FILE, 'r') as file:
+    blueprint = json.load(file)
+
+page_objects = []
+for page in blueprint['pages']:
+    row_objects = []
+    for row in page['rows']:
+        key_objects = []
+        for key in row['keys']:
+            tempaction = key['label'] #temporarily work with single letters
+            key_objects.append(Key(key['label'], tempaction))
+        row_objects.append(KeyboardRow(key_objects))
+    page_objects.append(KeyboardPage(row_objects))
+
+kb = Keyboard(page_objects)
+
