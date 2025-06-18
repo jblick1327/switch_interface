83bf9fe4718aa9a3734687d067c660057392e51d
jblick1327 <jblick1327@gmail.com>
2025-06-18 16:13:54 -0400

    created virtual keyboard tkinter interface

--- kb_layout_io.py
+++ kb_layout_io.py
@@ -4,19 +4,32 @@ from kb_layout import Key, KeyboardRow, KeyboardPage, Keyboard
 FOLDER = None
 FILE = 'layouts/basic_test.json'
 
-with open(FILE, 'r') as file:
-    blueprint = json.load(file)
+def load_keyboard(path: str = FILE) -> Keyboard:
+    """Load a :class:`Keyboard` definition from ``path``."""
+    with open(path, 'r') as file:
+        blueprint = json.load(file)
 
-page_objects = []
-for page in blueprint['pages']:
-    row_objects = []
-    for row in page['rows']:
-        key_objects = []
-        for key in row['keys']:
-            tempaction = key['label'] #temporarily work with single letters
-            key_objects.append(Key(key['label'], tempaction))
-        row_objects.append(KeyboardRow(key_objects))
-    page_objects.append(KeyboardPage(row_objects))
+    page_objects = []
+    for page in blueprint['pages']:
+        row_objects = []
+        for row in page['rows']:
+            key_objects = []
+            for key in row['keys']:
+                key_objects.append(
+                    Key(
+                        key['label'],
+                        key.get('action'),
+                        key.get('dwell'),
+                    )
+                )
+            row_objects.append(
+                KeyboardRow(
+                    key_objects,
+                    stretch=row.get('stretch', True),
+                )
+            )
+        page_objects.append(KeyboardPage(row_objects))
 
-kb = Keyboard(page_objects)
+    return Keyboard(page_objects)
 
+kb = load_keyboard(FILE)
